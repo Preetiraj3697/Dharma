d91d3c5c6abf4a9d6d6ff606510736b69a6df7f9
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-29 22:24:05 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,26 @@ function App() {
   const dispatch = useDispatch();
   const {url} = useSelector((state) =>state.home)
   console.log('url: ', url);
+
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular")
+  const fetchApiConfig= () => {
+    fetchDataFromApi("/configuration")
     .then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res))
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url))
     });
   };
+
   return (
     <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:mediaType/:id" element={<Details />} />
@@ -36,7 +43,7 @@ function App() {
             <Route path="/explore/:mediaType" element={<Explore />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
     </BrowserRouter>
 );
 }
