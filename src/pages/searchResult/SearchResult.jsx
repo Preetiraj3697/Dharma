9e7b5f86300261d2c28bd26babadbc76f3846623
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-31 00:00:13 +0530

    add searchResult

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-import './style.scss';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import "./style.scss";
+
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+// import MovieCard from "../../components/movieCard/MovieCard";
+// import Spinner from "../../components/spinner/Spinner";
+import noResults from "../../assets/no-results.png";
 const SearchResult = () => {
+  const [data,setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const {query} = useParams();
+
+  const fetchInitailData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).them((res)=>{
+      setData(res);
+      setPageNum((prev)=> prev + 1)
+      setLoading(false);
+    })
+  }
+  useEffect(()=>{
+    fetchInitailData();
+  },[query])
   return (
-    <div>
+    <div className="searchResultsPage">
       SearchResult
     </div>
   )
